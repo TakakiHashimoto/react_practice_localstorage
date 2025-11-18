3e0a87c2a3f9d22f359fe8a6a08a89e3b6747cae
gooko <takakihashimotogoo@gmail.com>
2025-11-18 14:52:48 -0800

    add try catch parse

--- local_storage_practice/src/App.tsx
+++ local_storage_practice/src/App.tsx
@@ -15,8 +15,12 @@ function App() {
  
   // First check if there is any cached data in local storage and if there is, set that value as initial value, else, set empty array
     const [toDoItem, setToDoItem] = useState<ToDoItem[]>(()=>{
+      try {
       const storedData = localStorage.getItem("todoItem");
       return storedData? JSON.parse(storedData): [];
+      } catch{
+        return [];
+      }
     });
 
     // Everytime an item is added to an array, store them in local storage. localStorage only accepts string = JSON.stringfy()
